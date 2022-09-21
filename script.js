2a4867822c47f4c3d36484f568973a22ebca8321
maxgonick <maxwellgonick@g.ucla.edu>
2022-09-21 15:15:11 -0700

    hamburger menu responsive positioning

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const hamburger = document.querySelector(".hamburger");
 const navHolder = document.querySelector(".nav-holder");
 const navLink = document.querySelectorAll(".nav-holder > li > a");
+let height = document.querySelector("nav").offsetHeight;
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -8,10 +9,15 @@ navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
 function closeMenu() {
   hamburger.classList.remove("active");
-  navMenu.classList.remove("active");
+  navHolder.classList.remove("active");
 }
 
 function mobileMenu() {
   hamburger.classList.toggle("active");
   navHolder.classList.toggle("active");
+  if (navHolder.getAttribute("class").includes("active")) {
+    navHolder.style.top = `${height}px`;
+  } else {
+    navHolder.style.top = `-100%`;
+  }
 }
