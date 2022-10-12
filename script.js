96dcfb04974d2b65a9609ac8cd55c67697e353db
maxgonick <maxwellgonick@g.ucla.edu>
2022-10-11 22:07:53 -0700

    mobile hamburger menu resizing

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-const hamburger = document.querySelector(".hamburger");
-const navHolder = document.querySelector(".nav-holder");
-const navLink = document.querySelectorAll(".nav-holder > li > a");
-let height = document.querySelector("nav").offsetHeight;
+window.addEventListener("load", (event) => {
+  const hamburger = document.querySelector(".hamburger");
+  const navHolder = document.querySelector(".nav-holder");
+  const navLink = document.querySelectorAll(".nav-holder > li > a");
+  let height = document.querySelector("nav").offsetHeight;
 
-hamburger.addEventListener("click", mobileMenu);
+  hamburger.addEventListener("click", mobileMenu);
 
-navLink.forEach((n) => n.addEventListener("click", closeMenu));
+  navLink.forEach((n) => n.addEventListener("click", closeMenu));
 
-function closeMenu() {
-  hamburger.classList.remove("active");
-  navHolder.classList.remove("active");
-}
-
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navHolder.classList.toggle("active");
-  if (navHolder.getAttribute("class").includes("active")) {
-    navHolder.style.top = `${height}px`;
-  } else {
-    navHolder.style.top = `-100%`;
+  function closeMenu() {
+    hamburger.classList.remove("active");
+    navHolder.classList.remove("active");
   }
-}
+
+  function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navHolder.classList.toggle("active");
+    if (navHolder.getAttribute("class").includes("active")) {
+      navHolder.style.top = `${height}px`;
+    } else {
+      navHolder.style.top = `-100%`;
+    }
+  }
+});
